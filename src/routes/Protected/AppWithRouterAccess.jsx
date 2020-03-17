9fe1c4b527727889d60c4739fe03c4c8234a0d15
hyde <rmlee0003@gmail.com>
2020-03-16 22:14:04 -0400

    fixed import errors

--- src/routes/Protected/AppWithRouterAccess.jsx
+++ src/routes/Protected/AppWithRouterAccess.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, useHistory } from 'react-router-dom';
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
-import RegisterForm from './RegisterForm';
-import Home from './Home';
-import Login from './Login';
+import RegisterForm from '../Signup/register';
+import Login from '../Signup/Login';
 import Dashboard from './dashboard'
 
-import NavBar from './NavBar';
 
 const AppWithRouterAccess = () => {
     const history = useHistory();
@@ -20,8 +18,6 @@ const AppWithRouterAccess = () => {
             redirectUri={window.location.origin + '/implicit/callback'}
             onAuthRequired={onAuthRequired}
             pkce={true} >
-            <NavBar />
-            <Route path='/' exact={true} component={Home} />
             <SecureRoute path='/dashboard' component={Dashboard} />
             <Route path='/implicit/callback' component={LoginCallback} />
             <Route exact path="/register" component={RegisterForm} />
