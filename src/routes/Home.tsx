9da6029a62687cb66dcc3b48e072dc681135a019
Kevin <kevincarr632@gmail.com>
2020-03-18 15:02:10 -0500

    created home component for later use

---
+++ src/routes/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      // this is where we will display information related to the app
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
