657b1e0c02ead1065af5ecebf78e4479c6f5eb95
Kevin <kevincarr632@gmail.com>
2020-03-18 14:55:56 -0500

    placed config for okta here

---
+++ src/utils/config.js
@@ -0,0 +1,12 @@
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || "{clientId}";
+const ISSUER =
+  process.env.REACT_APP_ISSUER || "https://{yourOktaDomain}.com/oauth2/default";
+
+export default {
+  oidc: {
+    issuer: ISSUER,
+    clientId: CLIENT_ID,
+    redirectUri: window.location.origin + "/implicit/callback",
+    pkce: true
+  }
+};
