f2bc9af7ff2b8e19b5ba17a46c91069f9f3eaf03
spettigrew <sara.pettigrew5@gmail.com>
2020-03-23 15:38:17 -0600

    Fixed styling

--- src/routes/footer.tsx
+++ src/routes/footer.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Styled from "styled-components";
 
 const FooterDiv = Styled.div`
-width:100%;
-background:black;
+width:100% !important;
+background:#08A6C9;
 color:white;
 height:40px;
 z-index:2;
 bottom:0 !important;
-position:fixed !important;
+position: fixed;
+text-align: center;
 
 `;
 
