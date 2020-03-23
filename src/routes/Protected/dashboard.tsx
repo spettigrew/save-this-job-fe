00b9d50431f1ef24aaf793f68790c9a4f513519a
spettigrew <sara.pettigrew5@gmail.com>
2020-03-23 15:45:26 -0600

    Added token for localstorage

--- src/routes/Protected/dashboard.tsx
+++ src/routes/Protected/dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import store from "store";
 import { DashCard } from "./card";
 import { Starwars } from "../../../index";
 
@@ -8,7 +9,16 @@ interface StarwarsArr extends Array<Starwars> {}
 const Dashboard = () => {
   const [users, setUsers] = useState<StarwarsArr>();
 
+  // This is used for the purpose of the chrome extension to authenticate users once they login
+  const setCredForExtension = () => {
+    const email = store.get("okta-token-storage").idToken.claims.email;
+    const token = store.get("okta-token-storage").accessToken.accessToken;
+    localStorage.setItem("email", email);
+    localStorage.setItem("token", token);
+  };
+
   useEffect(() => {
+    setCredForExtension();
     axios
       .get("https://cors-anywhere.herokuapp.com/https://swapi.co/api/people")
       .then(res => {
