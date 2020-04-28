31054b704abf6314235e54a2e655492ee486b612
Kevin <kevincarr632@gmail.com>
2020-04-27 19:43:43 -0500

    changes env files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,11 @@
-export let BASE_API_URL;
-if (process.env.NODE_ENV === "development") {
-  return (BASE_API_URL = "http://localhost:8080");
-}
-if (process.env.NODE_ENV === "staging") {
-  return (BASE_API_URL = "https://staging-save-this-job.herokuapp.com");
-}
-if (process.env.NODE_ENV === "production") {
-  return (BASE_API_URL = "https://save-this-job.herokuapp.com");
+export function baseApi() {
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:8080";
+  }
+  if (process.env.NODE_ENV === "staging") {
+    return "https://staging-save-this-job.herokuapp.com";
+  }
+  if (process.env.NODE_ENV === "production") {
+    return "https://save-this-job.herokuapp.com";
+  }
 }
