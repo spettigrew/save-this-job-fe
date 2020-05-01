4b34f02e5600c4a7687274b3e8552d69a182ba7d
roger-lee <rmlee0003@gmail.com>
2020-04-30 21:34:03 -0500

    fixed error, added import

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Footer from "../routes/footer";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
