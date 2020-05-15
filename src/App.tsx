ced0bcc12d99624774decd62c400ebd6ee22ca8e
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-05-14 21:18:03 -0500

    undo jonathans boo boo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppWithRouterAccess from './routes/Protected/AppWithRouterAccess';
-import '~semantic-ui-less/semantic.less';
+import 'semantic-ui-less/semantic.less';
 function App() {
   return (
     <div>
