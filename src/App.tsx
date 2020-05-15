f8858cbe6dcea6308e596ba76f981da6d1243282
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-05-14 21:49:58 -0400

    Added google analytic support with ReactGA

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppWithRouterAccess from "./routes/Protected/AppWithRouterAccess";
 import "semantic-ui-less/semantic.less";
+import ReactGA from "react-ga";
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+
+    // pageviews
+    ReactGA.pageview(window.location.pathname);
+    console.log("fire react-ga");
+  }, []);
   return (
     <div>
       <AppWithRouterAccess />
